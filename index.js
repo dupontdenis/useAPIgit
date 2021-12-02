a871967f1a69abfe82f858768caa0d1758d0eb9f
Denis Dupont <denis.dupont@univ-evry.fr>
2021-12-02 14:10:21 +0100

    query

--- index.js
+++ index.js
@@ -13,12 +13,25 @@ app.get('/users/:id', async (req, res) => {
 
     try {
         debug(`id = ${req.params.id}`);
-        const { data:user } = await octokit.request(`GET /users/${req.params.id}`, {
-            username: 'username'
+        const { data: user } = await octokit.request(`GET /users/{username}`, {
+            username: req.params.id,
         });
         debug(`data  = ${user.login}, ${user.avatar_url}`);
 
         res.render('index', { user });
+    } catch (error) {
+        console.log(error)
+    }
+
+});
+
+app.get('/users/:id/repos', async (req, res) => {
+
+    try {
+        debug(`id = ${req.params.id}`);
+        debug(`sort = ${req.url}`);
+        const { data: repos } = await octokit.request(`GET ${req.url}`);
+        res.render('repos', { repos });
     } catch (error) {
         console.log(error)
     }

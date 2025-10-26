15f74a3483c0a987565c68ccdf2888b903af6532
denis dupont <denis.dupont@univ-evry.fr>
2025-10-26 16:32:53 +0100

    update app

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require("express");
-const path = require("path");
-const debug = require("debug")("git");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import debugModule from "debug";
+
+const debug = debugModule("git");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
